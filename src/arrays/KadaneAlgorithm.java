4c47fda49aaa8de4194466383c5094d2d66837af
lovelesh <lovelesh.s@tringapps.com>
2018-07-03 23:52:10 -0700

    Implemented kadanes algorithm

--- src/arrays/KadaneAlgorithm.java
+++ src/arrays/KadaneAlgorithm.java
@@ -1,6 +1,33 @@
 package arrays;
 
+/**
+ * Kadane's Algorithm for finding largest sum contiguous sub array
+ */
+
 public class KadaneAlgorithm {
+
+    public static void main(String...args) {
+        int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+        System.out.println(new KadaneAlgorithm().kadaneAlgorithm(arr));
+    }
+
+
+    public int kadaneAlgorithm(int arr[]) {
+        int size = arr.length;
+        if(size == 0)
+            return 0;
+        int currMax = arr[0];
+        int globalMax = arr[0];
+        for (int i = 1; i < size; i++) {
+            currMax += arr[i];
+            if(currMax > globalMax)
+                globalMax = currMax;
+            if(currMax < 0)
+                currMax = 0;
+
+        }
+        return globalMax;
+    }
 
 
 }
