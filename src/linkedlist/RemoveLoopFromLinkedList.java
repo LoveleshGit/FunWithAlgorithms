af84629c622e3b83dea902bed1f5b4b0bdb62740
Lovelesh <lovelesh90@gmail.com>
2018-07-05 22:59:19 +0530

    implemented removing loop from linked list

--- src/linkedlist/RemoveLoopFromLinkedList.java
+++ src/linkedlist/RemoveLoopFromLinkedList.java
@@ -1,5 +1,8 @@
 package linkedlist;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RemoveLoopFromLinkedList {
 
     public static void main(String...args) {
@@ -11,12 +14,23 @@ public class RemoveLoopFromLinkedList {
         list.insert(4);
         head = list.insert(5);
         list.createLoopAtLastKth(2);
+        new RemoveLoopFromLinkedList().removeLoop(head);
+        list.print();
         System.out.println(new DetectLoopinLinkedList().detectLoop(head));
     }
 
     public void removeLoop(Node head) {
 
-
-
+        Set<Integer> set = new HashSet<Integer>();
+        Node temp = head;
+        int key;
+        while(temp != null && temp.next != null) {
+            key = temp.next.data;
+            if(set.contains(key)) {
+                temp.next=null;
+            } else
+                set.add(key);
+            temp = temp.next;
+        }
     }
 }
