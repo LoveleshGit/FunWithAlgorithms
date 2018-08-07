2c85885db58a0e78eb8eec03eaef1b69f25cd41c
lovelesh <lovelesh.s@tringapps.com>
2018-08-07 01:49:08 -0700

    Mirror Images Implemented

--- src/trees/MirrorImage.java
+++ src/trees/MirrorImage.java
@@ -1,46 +1,68 @@
 package trees;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MirrorImage {
 
+    public static void main(String... args) {
 
-    public int findMirrorImg(TreeNode root, int key) {
+        TreeNode tree1 = new TreeNode(8);
+        tree1.insert(4);
+        tree1.insert(10);
+        tree1.insert(2);
+        tree1.insert(9);
+        tree1.insert(19);
+
+        int mirror = new MirrorImage().findMirrorImgIterative(tree1,9);
+        System.out.println("Mirror Image: "+mirror);
 
     }
 
-    public TreeNode findMirrorImgIterative(TreeNode root, int key) {
+    public int findMirrorImgIterative(TreeNode root, int key) {
 
         if(root == null)
-            return  root;
+            return -1;
 
+        if(root.getData() == key)
+            return key;
 
+        ArrayList<Integer> treeArray = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        ((LinkedList<TreeNode>) queue).add(root);
 
+        int nodeCount = 1;
 
+        while(true) {
+            nodeCount = queue.size();
+            if(nodeCount == 0)
+                return -1;
+            treeArray = new ArrayList<>();
+
+            while(nodeCount > 0) {
+                TreeNode tempNode = queue.poll();
+                if(tempNode.getLeft() != null) {
+                    ((LinkedList<TreeNode>) queue).add(tempNode.getLeft());
+                    treeArray.add(tempNode.getLeft().getData());
+                } else {
+                    treeArray.add(-1);
+                }
+
+                if(tempNode.getRight() != null) {
+                    ((LinkedList<TreeNode>) queue).add(tempNode.getRight());
+                    treeArray.add(tempNode.getRight().getData());
+                } else {
+                    treeArray.add(-1);
+                }
+                nodeCount--;
+            }
+
+            System.out.println(treeArray.toString());
+            if(treeArray.contains(key)) {
+                int index = treeArray.indexOf(key);
+                return treeArray.get((treeArray.size() - 1) - index);
+            }
+        }
     }
-
-
-
-
-
 }
-
-
-//    public int findMirrorImg(int key,TreeNode left,TreeNode right) {
-//
-//        if(left == null || right == null)
-//            return 0;
-//
-//        if(left.getData() == key) {
-//            return right.getData();
-//        }
-//
-//        if(right.getData() == key) {
-//            return left.getData();
-//        }
-//
-//        int mirr_img = findMirrorImg(key,left.getLeft(),right.getRight());
-//
-//        if(mirr_img > 0)
-//            return mirr_img;
-//
-//        findMirrorImg(key,left.getRight(),right.getLeft());
-//    }
